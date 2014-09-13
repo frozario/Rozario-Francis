724517beb486be53f0bb563c7d2e8ad5acfd5c4b
Francis Rozario <f.rozario@hotmail.com>
2014-09-12 22:52:58 -0400

    Added both readme and php file

---
+++ README.md
@@ -0,0 +1,16 @@
+Live URL:
+
+http://p1.frozario.me
+
+Description:
+
+Simple HTML/CSS Page.
+
+Details for teaching team:
+
+No login required.
+The file frozario.php was used to generate the word list.
+
+Outside Code:
+
+No code used.
---
+++ frozario.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+
+	<style type="text/css">
+	
+		h {text-align: center;}
+		
+		</style>
+
+</head>
+
+<body>
+
+<h>
+<h1>	
+
+Francis Rozario
+
+<br />
+
+Dynamic Web Apps Portfolio
+
+</h1>
+
+</h>
+<center><img src="Image.jpg"></center>
+
+<h>
+
+<h3><a href='http://p2.frozario.me'>View Project 2</a></h3><br />
+<a href='http://github.com/frozario/p2'>P2 on Github</a>
+
+<h3><a href='http://p3.frozario.me'>View Project 3</a></h3><br />
+<a href='http://github.com/frozario/p3'>P3 on Github</a>
+
+<h3><a href='http://p4.frozario.me'>View Project 4</a></h3><br />
+<a href='http://github.com/frozario/p4'>P4 on Github</a>
+
+</h>
+
+<h>
+<h2>In 2+ sentences describe who you are and what you do.</h2><br />
+<h4>Francis Rozario, born and brought up in India. Moved to US in 2006, graduated from Rutgers University and presently a Risk Consulting for Crowe Horwath LLP an accounting firm in NY.</h4>
+
+<h2>Why are you taking this course?</h2><br />
+<h4>My job requires me to learn more about Information Technology. Since my Bachelors was in Accounting and Finance and I have no knowledge in Information Technology, I am taking these classes to expand my skillsets and also hoping to get admitted to Harvard Extension School.</h4>
+
+<h2>What previous experience do you have with programming?</h2><br />
+<h4>I have no experience with programming.</h4>
+
+<h2>Are you working on a Mac, PC, or some other platform?</h2><br />
+<h4>I am working in Mac.</h4>
+
+<h2>Anything else we should know?</h2><br />
+<h4>Travelling for work most of the time, so will try to keep up with the lectures and Home Works as much as possible :)</h4>
+
+</body>
+
+</html>
